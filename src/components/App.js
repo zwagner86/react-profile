55b40b013118e435c9b2cf5faad2d8bed6c9f6f3
zwagner <Zachary.Wagner@charter.com>
2016-11-21 16:15:54 -0600

    add assets and basic app structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './App.css';
 
 class App extends Component {
