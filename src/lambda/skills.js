21140a6c536e99fb69b9910ea06eeaad01ffd125
Zach Wagner <zach.wagner@spothero.com>
2019-01-27 17:11:48 -0600

    update headers

--- src/lambda/skills.js
+++ src/lambda/skills.js
@@ -1,8 +1,14 @@
 import skillData from '../data/skills';
 
+const headers = {
+    "Access-Control-Allow-Origin" : "*",
+    "Vary": "Origin"
+};
+
 export function handler(event, context, callback) {
     callback(null, {
         statusCode: 200,
+        headers,
         body: JSON.stringify({data: skillData})
     });
 }
